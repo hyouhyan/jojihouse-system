d313f8e09b41244d780de8592f9916d9ee4ef448
hyouhyan <hyouhyan@hyouhyan.com>
2025-03-26 01:00:03 +0900

    feat: requirements.txtにpython-dotenvを追加し、user.pyを新規作成してPostgreSQL接続を設定

---
+++ entrance-system/user.py
@@ -0,0 +1,23 @@
+# postgresql
+import psycopg2
+import dotenv
+import os
+
+dotenv.load_dotenv()
+
+
+POSTGRES_USER = os.getenv("POSTGRES_USER")
+POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
+POSTGRES_DB = os.getenv("POSTGRES_DB")
+POSTGRES_HOST = os.getenv("POSTGRES_HOST")
+POSTGRES_PORT = os.getenv("POSTGRES_PORT")
+
+conn = psycopg2.connect(
+    dbname=POSTGRES_DB,
+    user=POSTGRES_USER,
+    password=POSTGRES_PASSWORD,
+    host=POSTGRES_HOST,
+    port=POSTGRES_PORT
+)
+
+cursor = conn.cursor()
