cbee4756bf1e5bba6f10b5f9c2fe606bd313b2d0
hyouhyan <hyouhyan@hyouhyan.com>
2025-03-26 17:25:30 +0900

    feat: ユーザーにロールを追加・削除する機能を追加

--- entrance-system/internal/repository/role_repo.go
+++ entrance-system/internal/repository/role_repo.go
@@ -98,3 +98,20 @@ func (r *RoleRepository) IsGuest(userID int) (bool, error) {
 	}
 	return false, nil
 }
+
+func (r *RoleRepository) AddRoleToUser(userID, roleID int) error {
+	_, err := r.db.Exec("INSERT INTO user_roles (user_id, role_id) VALUES ($1, $2)", userID, roleID)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (r *RoleRepository) RemoveRoleFromUser(userID, roleID int) error {
+	_, err := r.db.Exec("DELETE FROM user_roles WHERE user_id = $1 AND role_id = $2", userID, roleID)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
