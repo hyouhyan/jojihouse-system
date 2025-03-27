56780bae12c4df36f4b44edd47481d17882f3352
hyouhyan <hyouhyan@hyouhyan.com>
2025-03-28 00:55:34 +0900

    feat: RemainingEntriesLog構造体のフィールドにBSONタグを追加

--- entrance-system/internal/model/remaining_entries_log.go
+++ entrance-system/internal/model/remaining_entries_log.go
@@ -8,10 +8,10 @@ import (
 
 type RemainingEntriesLog struct {
 	ID              primitive.ObjectID `json:"id" bson:"_id,omitempty"`
-	UserID          int                `json:"user_id"`
-	PreviousEntries int                `json:"previous_entries"`
-	NewEntries      int                `json:"new_entries"`
-	Reason          string             `json:"reason"`
-	UpdatedBy       string             `json:"updated_by"`
-	UpdatedAt       time.Time          `json:"updated_at"`
+	UserID          int                `json:"user_id" bson:"user_id"`
+	PreviousEntries int                `json:"previous_entries" bson:"previous_entries"`
+	NewEntries      int                `json:"new_entries" bson:"new_entries"`
+	Reason          string             `json:"reason" bson:"reason"`
+	UpdatedBy       string             `json:"updated_by" bson:"updated_by"`
+	UpdatedAt       time.Time          `json:"updated_at" bson:"updated_at"`
 }
