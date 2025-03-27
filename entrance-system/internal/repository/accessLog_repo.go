b9782bb1765bc320361eed7f1ab1ff2a6d368bff
hyouhyan <hyouhyan@hyouhyan.com>
2025-03-28 00:23:46 +0900

    refactor: アクセスログ取得関数の共通部分をリファクタリング

--- entrance-system/internal/repository/accessLog_repo.go
+++ entrance-system/internal/repository/accessLog_repo.go
@@ -29,16 +29,27 @@ func (r *AccessLogRepository) CreateAccessLog(log *model.AccessLog) error {
 }
 
 func (r *AccessLogRepository) GetAccessLogs(lastID primitive.ObjectID) ([]model.AccessLog, error) {
+	// フィルターなしで全ログを取得
+	return r._findAccessLogs(bson.D{}, lastID, 50)
+}
+
+func (r *AccessLogRepository) GetAccessLogsByUserID(userID int, lastID primitive.ObjectID) ([]model.AccessLog, error) {
+	// `user_id` でフィルター
+	filter := bson.D{{Key: "user_id", Value: userID}}
+	return r._findAccessLogs(filter, lastID, 50)
+}
+
+// 共通の検索処理
+func (r *AccessLogRepository) _findAccessLogs(filter bson.D, lastID primitive.ObjectID, limit int64) ([]model.AccessLog, error) {
 	var logs []model.AccessLog
 
 	findOptions := options.Find()
-	findOptions.SetLimit(50)
+	findOptions.SetLimit(limit)
 	findOptions.SetSort(bson.D{{Key: "time", Value: 1}}) // `time` で昇順ソート
 
-	// lastID がゼロ値でなければ、それ以降のデータを取得
-	filter := bson.D{}
+	// lastID によるページネーション
 	if !lastID.IsZero() {
-		filter = bson.D{{Key: "_id", Value: bson.D{{Key: "$gt", Value: lastID}}}} // `_id` を基準にページネーション
+		filter = append(filter, bson.E{Key: "_id", Value: bson.D{{Key: "$gt", Value: lastID}}})
 	}
 
 	cursor, err := r.db.Collection("access_log").Find(context.Background(), filter, findOptions)
@@ -47,38 +58,6 @@ func (r *AccessLogRepository) GetAccessLogs(lastID primitive.ObjectID) ([]model.
 	}
 	defer cursor.Close(context.Background())
 
-	for cursor.Next(context.Background()) {
-		var log model.AccessLog
-		if err := cursor.Decode(&log); err != nil {
-			return nil, err
-		}
-
-		// タイムゾーンの変換
-		log.Time = log.Time.In(time.Local)
-
-		logs = append(logs, log)
-	}
-	return logs, nil
-}
-
-func (r *AccessLogRepository) GetAccessLogsByUserID(userID int, lastID primitive.ObjectID) ([]model.AccessLog, error) {
-	var logs []model.AccessLog
-
-	findOptions := options.Find()
-	findOptions.SetLimit(50)
-	findOptions.SetSort(bson.D{{Key: "time", Value: 1}}) // `time` で昇順ソート
-
-	// lastID がゼロ値でなければ、それ以降のデータを取得
-	filter := bson.D{{Key: "user_id", Value: userID}}
-	if !lastID.IsZero() {
-		filter = bson.D{{Key: "_id", Value: bson.D{{Key: "$gt", Value: lastID}}}} // `_id` を基準にページネーション
-	}
-
-	cursor, err := r.db.Collection("access_log").Find(context.Background(), filter)
-	if err != nil {
-		return nil, err
-	}
-	defer cursor.Close(context.Background())
 	for cursor.Next(context.Background()) {
 		var log model.AccessLog
 		if err := cursor.Decode(&log); err != nil {
