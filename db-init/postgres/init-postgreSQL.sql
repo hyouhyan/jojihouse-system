f879559e6877b87392712ae9352214ef38d3c0db
Hyouhyan <hyouhyan@hyouhyan.com>
2025-03-27 23:21:41 +0900

    feat: registered_atをタイムゾーン付きのタイムスタンプに変更

--- db-init/postgres/init-postgreSQL.sql
+++ db-init/postgres/init-postgreSQL.sql
@@ -5,7 +5,7 @@ CREATE TABLE users (
     barcode CHAR(13) UNIQUE NOT NULL,
     contact VARCHAR(255),
     remaining_entries INT DEFAULT 0 CHECK (remaining_entries >= 0),
-    registered_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    registered_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
     total_entries INT DEFAULT 0 CHECK (total_entries >= 0)
 );
 
