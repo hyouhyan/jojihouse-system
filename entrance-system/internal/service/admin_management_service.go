3f1aa27aaaa69fd9cbee79744a4889e08f2fa512
hyouhyan <hyouhyan@hyouhyan.com>
2025-03-28 22:00:28 +0900

    feat: admin_management_serviceの必要関数追加

--- entrance-system/internal/service/admin_management_service.go
+++ entrance-system/internal/service/admin_management_service.go
@@ -1,7 +1,10 @@
 package service
 
 import (
+	"jojihouse-entrance-system/internal/model"
 	"jojihouse-entrance-system/internal/repository"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 type AdminManagementService struct {
@@ -13,4 +16,36 @@ type AdminManagementService struct {
 
 func NewAdminManagementService(userRepository *repository.UserRepository, roleRepository *repository.RoleRepository, accessLogRepository *repository.AccessLogRepository, remainingEntriesLogRepository *repository.RemainingEntriesLogRepository) *AdminManagementService {
 	return &AdminManagementService{userRepository: userRepository, roleRepository: roleRepository, accessLogRepository: accessLogRepository, remainingEntriesLogRepository: remainingEntriesLogRepository}
+}
+
+func (s *AdminManagementService) CreateUser(user *model.User) (*model.User, error) {
+	return s.userRepository.CreateUser(user)
+}
+
+func (s *AdminManagementService) UpdateUser(user *model.User) error {
+	return s.userRepository.UpdateUser(user)
+}
+
+func (s *AdminManagementService) DeleteUser(userID int) error {
+	return s.userRepository.DeleteUser(userID)
+}
+
+func (s *AdminManagementService) IncreaseRemainingEntries(userID int, count int) error {
+	return s.userRepository.IncreaseRemainingEntries(userID, count)
+}
+
+func (s *AdminManagementService) AddRoleToUser(userID, roleID int) error {
+	return s.roleRepository.AddRoleToUser(userID, roleID)
+}
+
+func (s *AdminManagementService) RemoveRoleFromUser(userID, roleID int) error {
+	return s.roleRepository.RemoveRoleFromUser(userID, roleID)
+}
+
+func (s *AdminManagementService) GetAccessLogs(lastID primitive.ObjectID) ([]model.AccessLog, error) {
+	return s.accessLogRepository.GetAccessLogs(lastID)
+}
+
+func (s *AdminManagementService) GetRemainingEntriesLogs(lastID primitive.ObjectID) ([]model.RemainingEntriesLog, error) {
+	return s.remainingEntriesLogRepository.GetRemainingEntriesLogs(lastID)
 }
