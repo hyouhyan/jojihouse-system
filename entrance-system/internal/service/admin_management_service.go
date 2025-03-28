ba0366d3309eec3040e3b6583e475ef71a4e77bc
hyouhyan <hyouhyan@hyouhyan.com>
2025-03-28 23:10:34 +0900

    feat: 入場可能回数追加時にログを残すように

--- entrance-system/internal/service/admin_management_service.go
+++ entrance-system/internal/service/admin_management_service.go
@@ -3,6 +3,7 @@ package service
 import (
 	"jojihouse-entrance-system/internal/model"
 	"jojihouse-entrance-system/internal/repository"
+	"time"
 
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
@@ -30,8 +31,42 @@ func (s *AdminManagementService) DeleteUser(userID int) error {
 	return s.userRepository.DeleteUser(userID)
 }
 
-func (s *AdminManagementService) IncreaseRemainingEntries(userID int, count int) error {
-	return s.userRepository.IncreaseRemainingEntries(userID, count)
+// 入場可能回数の追加
+func (s *AdminManagementService) IncreaseRemainingEntries(userID int, count int, reason string, updatedBy string) error {
+	// 変更前のデータ取得
+	user, err := s.userRepository.GetUserByID(userID)
+	if err != nil {
+		return err
+	}
+
+	// 入場可能回数 追加
+	err = s.userRepository.IncreaseRemainingEntries(userID, count)
+	if err != nil {
+		return err
+	}
+
+	// 変更前の回数
+	prevCount := user.Remaining_entries
+
+	// 再度取得
+	user, err = s.userRepository.GetUserByID(userID)
+	if err != nil {
+		return err
+	}
+	// 変更後の回数
+	newCount := user.Remaining_entries
+
+	// ログに保存
+	log := &model.RemainingEntriesLog{
+		UserID:          userID,
+		PreviousEntries: prevCount,
+		NewEntries:      newCount,
+		Reason:          reason,
+		UpdatedBy:       updatedBy,
+		UpdatedAt:       time.Now(),
+	}
+
+	return s.remainingEntriesLogRepository.CreateRemainingEntriesLog(log)
 }
 
 func (s *AdminManagementService) AddRoleToUser(userID, roleID int) error {
