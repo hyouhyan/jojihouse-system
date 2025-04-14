dcba3c0039ab686cd952d4db9cdde4171d2e7e9d
hyouhyan <hyouhyan@hyouhyan.com>
2025-04-15 01:00:25 +0900

    refactor: user情報の取得をsqlxに移行

--- entrance-system/internal/model/user.go
+++ entrance-system/internal/model/user.go
@@ -3,13 +3,13 @@ package model
 import "time"
 
 type User struct {
-	ID                int
-	Name              string
-	Description       string
-	Barcode           string
-	Contact           string
-	Remaining_entries int
-	Registered_at     time.Time
-	Total_entries     int
-	Allergy           string
+	ID                int       `db:"id"`
+	Name              string    `db:"name"`
+	Description       string    `db:"description"`
+	Barcode           string    `db:"barcode"`
+	Contact           string    `db:"contact"`
+	Remaining_entries int       `db:"remaining_entries"`
+	Registered_at     time.Time `db:"registered_at"`
+	Total_entries     int       `db:"total_entries"`
+	Allergy           string    `db:"allergy"`
 }
