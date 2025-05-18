983c44673535b447f28f4cbf8196753063a97c13
hyouhyan <hyouhyan@hyouhyan.com>
2025-05-19 03:41:06 +0900

    fix: アレルギーはnullを許可してたので、currentUsersのmodelでポインタに変更

--- entrance-system/internal/model/current_user.go
+++ entrance-system/internal/model/current_user.go
@@ -6,5 +6,5 @@ type CurrentUser struct {
 	UserID    int       `json:"user_id" db:"user_id"`
 	Name      string    `json:"user_name" db:"name"`
 	EnteredAt time.Time `json:"entered_at" db:"entered_at"`
-	Allergy   string    `json:"allergy" db:"allergy"`
+	Allergy   *string    `json:"allergy" db:"allergy"`
 }
