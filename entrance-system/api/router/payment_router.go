f83669bafe7a9845e60bb2dcab5ae44caf70d768
hyouhyan <hyouhyan@hyouhyan.com>
2025-06-10 14:30:17 +0900

    feat; paymentのrouter作成

--- entrance-system/api/router/payment_router.go
+++ entrance-system/api/router/payment_router.go
@@ -10,6 +10,7 @@ func SetupPaymentRoutes(router *gin.Engine, handler *handler.PaymentHandler) {
 	group := router.Group("/payment")
 	{
 		group.GET("", handler.GetAllPaymentLogs)
+		group.POST("", handler.CreatePaymentLog)
 		group.GET("/monthly", handler.GetMonthlyPaymentLogs)
 	}
 }
