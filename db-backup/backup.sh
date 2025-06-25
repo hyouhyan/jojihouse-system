12e70373a68a008863db8ef4bbecb7aace06acb8
hyouhyan <hyouhyan@hyouhyan.com>
2025-06-26 01:21:14 +0900

    fix: バックアップスクリプト内でdbのhostを指定

--- db-backup/backup.sh
+++ db-backup/backup.sh
@@ -15,12 +15,12 @@ echo "--- Starting backup at ${DATE} ---"
 # --- PostgreSQLのバックアップ ---
 echo "Dumping PostgreSQL database..."
 # 環境変数はOfeliaの定義から引き継ぐか、ここで指定します
-pg_dumpall -U ${POSTGRES_USER} > "${BACKUP_DIR}/postgres_backup_${DATE}.sql"
+pg_dumpall -U ${POSTGRES_USER} -h ${POSTGRES_HOST}  > "${BACKUP_DIR}/postgres_backup_${DATE}.sql"
 echo "PostgreSQL dump successful."
 
 # --- MongoDBのバックアップ ---
 echo "Dumping MongoDB database..."
-mongodump --username ${MONGO_INITDB_ROOT_USERNAME} --password ${MONGO_INITDB_ROOT_PASSWORD} --authenticationDatabase admin --archive > "${BACKUP_DIR}/mongodb_backup_${DATE}.archive"
+mongodump --host ${MONGO_HOST} --username ${MONGO_INITDB_ROOT_USERNAME} --password ${MONGO_INITDB_ROOT_PASSWORD} --authenticationDatabase admin --archive > "${BACKUP_DIR}/mongodb_backup_${DATE}.archive"
 echo "MongoDB dump successful."
 
 echo "--- Backup completed successfully ---"
