7b12531700882c0ef52d160a30b7a20cf6e3e81f
hyouhyan <hyouhyan@hyouhyan.com>
2025-08-26 00:05:23 +0900

    feat: 全ユーザを取得する関数追加

--- script/consistency-checker/main.go
+++ script/consistency-checker/main.go
@@ -1,6 +1,12 @@
 package main
 
-import "jojihouse-system-consistency-checker/database"
+import (
+	"fmt"
+	"jojihouse-system-consistency-checker/database"
+	"jojihouse-system-consistency-checker/model"
+
+	"github.com/jmoiron/sqlx"
+)
 
 func main() {
 	// DBへ接続
@@ -9,4 +15,27 @@ func main() {
 
 	database.ConnectMongo()
 	defer database.CloseMongo()
+
+	users, err := GetAllUsers(database.PostgresDB)
+	if err != nil {
+		fmt.Printf("Error fetching users: %v\n", err)
+		return
+	}
+
+	for _, user := range users {
+		fmt.Println(*user.Name)
+	}
 }
+
+func GetAllUsers(db *sqlx.DB) ([]model.User, error) {
+	var users []model.User
+	err := db.Select(&users, "SELECT * FROM users ORDER BY number")
+
+	if err != nil {
+		return nil, fmt.Errorf("failed to get users: %w", err)
+	}
+
+	return users, nil
+}
+
+
---
+++ script/consistency-checker/model/user.go
@@ -0,0 +1,16 @@
+package model
+
+import "time"
+
+type User struct {
+	ID                *int       `db:"id"`
+	Name              *string    `db:"name"`
+	Description       *string    `db:"description"`
+	Barcode           *string    `db:"barcode"`
+	Contact           *string    `db:"contact"`
+	Remaining_entries *int       `db:"remaining_entries"`
+	Registered_at     *time.Time `db:"registered_at"`
+	Total_entries     *int       `db:"total_entries"`
+	Allergy           *string    `db:"allergy"`
+	Number            *int       `db:"number"`
+}
