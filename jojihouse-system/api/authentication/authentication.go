9781dac54804cbd01259c949d86b44f06205381f
hyouhyan <hyouhyan@hyouhyan.com>
2025-08-27 22:56:18 +0900

    feat: jwtトークンの発行と検証の関数書いた

--- jojihouse-system/api/authentication/authentication.go
+++ jojihouse-system/api/authentication/authentication.go
@@ -1,6 +1,13 @@
 package authentication
 
-import "jojihouse-system/internal/service"
+import (
+	"fmt"
+	"jojihouse-system/internal/service"
+	"os"
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+)
 
 type Authentication struct {
 	userPortalService *service.UserPortalService
@@ -8,4 +15,38 @@ type Authentication struct {
 
 func NewAuthentication(userPortalService *service.UserPortalService) *Authentication {
 	return &Authentication{userPortalService: userPortalService}
+}
+
+func (a *Authentication) CreateJWTToken(userID int) (tokenStr string, err error) {
+	Env_load()
+
+	// トークン発行
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"sub": userID,
+		"exp": time.Now().Add(time.Hour * 24).Unix(),
+	})
+
+	// Stringに変換
+	tokenStr, err = token.SignedString([]byte(os.Getenv("JWT_SECRET")))
+	if err != nil {
+		return "", fmt.Errorf("failed to convert token to string: %v", err)
+	}
+
+	return tokenStr, nil
+}
+
+func (a *Authentication) VerifyJWTToken(tokenStr string) (ok bool, err error) {
+	Env_load()
+
+	// トークンの検証
+	token, err := jwt.Parse(tokenStr, func(token *jwt.Token) (interface{}, error) {
+		return []byte(os.Getenv("JWT_SECRET")), nil
+	})
+	if err != nil {
+		return false, fmt.Errorf("failed to verify jwt token: %v", err)
+	} else if !token.Valid {
+		return false, fmt.Errorf("invalid token")
+	}
+
+	return true, nil
 }
