a2b81f3b215586badb79eadf9ea97c2be11f3f07
hyouhyan <hyouhyan@hyouhyan.com>
2025-10-06 13:31:10 +0900

    feat: paymentlogのレスポンスにis_deleted関連の情報追加

--- jojihouse-system/api/model/response/payment_response.go
+++ jojihouse-system/api/model/response/payment_response.go
@@ -5,13 +5,16 @@ import (
 )
 
 type PaymentLog struct {
-	ID          string    `json:"id"`
-	UserID      int       `json:"user_id"`
-	UserName    string    `json:"user_name"`
-	Time        time.Time `json:"time"`
-	Description string    `json:"description"`
-	Amount      int       `json:"amount"`
-	Payway      string    `json:"payway"`
+	ID          string     `json:"id"`
+	UserID      int        `json:"user_id"`
+	UserName    string     `json:"user_name"`
+	Time        time.Time  `json:"time"`
+	Description string     `json:"description"`
+	Amount      int        `json:"amount"`
+	Payway      string     `json:"payway"`
+	IsDeleted   bool       `json:"is_deleted"`
+	DeletedBy   *int       `json:"deleted_by"`
+	DeletedAt   *time.Time `json:"deleted_at"`
 }
 
 type MonthlyPaymentLog struct {
